aa623940c955aa91100fe1d2e88adfd5502b8cf7
cpeixin <421391491@qq.com>
2020-02-15 11:36:50 +0800

    nothing

--- queen/array_queue.py
+++ queen/array_queue.py
@@ -1,22 +1,47 @@
-class ArrayQueue(object):
-    def __init__(self, size):
-        self.size = size
-        self.array = []
+from typing import Optional
+class ArrayQueue:
 
-        self.head = 0
-        self.tail = 0
+    def __init__(self, capacity: int):
+        self._items = []
+        self._capacity = capacity
+        self._head = 0
+        self._tail = 0
 
-    def enqueue(self, data):
-        """入队判满"""
-        if self.tail-self.head+1 == self.size:
+    def enqueue(self, item: int) -> bool:
+        if self._tail == self._capacity:
+            if self._head == 0:
+                return False
+            else:
+                for i in range(0, self._tail - self._head):
+                    self._items[i] = self._items[i + self._head]
+                self._tail = self._tail - self._head
+                self._head = 0
+
+        self._items.insert(self._tail, item)
+        self._tail += 1
+        return True
+
+    def dequeue(self) -> Optional[str]:
+        if self._head != self._tail:
+            item = self._items[self._head]
+            self._head += 1
+            return item
+        else:
             return None
-        self.array.append(data)
-        self.tail = self.tail + 1
 
-    def dequeue(self):
-        """出队判空"""
-        if self.tail == self.head:
-            return None
-        data = self.array[self.head]
-        self.head = self.head+1
-        return data
+    def __repr__(self) -> str:
+        return " ".join(item for item in self._items[self._head: self._tail])
+
+if __name__ == '__main__':
+    array_queue = ArrayQueue(5)
+    array_queue.enqueue(1)
+    array_queue.enqueue(2)
+    array_queue.enqueue(3)
+    array_queue.enqueue(4)
+    array_queue.enqueue(5)
+    # array_queue.enqueue(6)
+
+
+    print(array_queue._head)
+    print(array_queue._tail)
+
