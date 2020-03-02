d3cf988028854ff47f7e17d05735eb748f89772b
brent <421391491@qq.com>
2020-03-02 18:46:59 +0800

    reverse_lined

--- linklist/reverse_linked_iteration.py
+++ linklist/reverse_linked_iteration.py
@@ -2,12 +2,17 @@
 import json
 
 
-class ListNode(object):
+class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-
-
+"""
+双指针迭代
+我们可以申请两个指针，第一个指针叫 pre，最初是指向 null 的。
+第二个指针 cur 指向 head，然后不断遍历 cur。
+每次迭代到 cur，都将 cur 的 next 指向 pre，然后 pre 和 cur 前进一位。
+都迭代完了(cur 变成 null 了)，pre 就是最后一个节点了。
+"""
 
 class Solution:
     def reverseList(self, head):
@@ -15,16 +20,24 @@ class Solution:
         :type head: ListNode
         :rtype: ListNode
         """
-        current = head
-        prev = None
-        while current:
-            current.next, prev, current = prev, current, current.next
-        return prev
+        pre = None
+        cur = head
+
+        while cur != None:
+            lat = cur.next
+            cur.next = pre
+            pre = cur
+            cur = lat
+
+        return pre
+
+def stringToIntegerList(input):
+    return json.loads(input)
 
 
 def stringToListNode(input):
     # Generate list from the input
-    numbers = json.loads(input)
+    numbers = stringToIntegerList(input)
 
     # Now convert that list into linked list
     dummyRoot = ListNode(0)
@@ -50,14 +63,15 @@ def listNodeToString(node):
 
 def main():
     import sys
+    import io
     def readlines():
-        for line in sys.stdin:
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
             yield line.strip('\n')
 
     lines = readlines()
     while True:
         try:
-            line = lines.next()
+            line = next(lines)
             head = stringToListNode(line)
 
             ret = Solution().reverseList(head)
