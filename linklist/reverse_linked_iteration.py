d336b909cb66ca64bb937592eb2ffeba1d77be03
brent <421391491@qq.com>
2020-03-02 18:47:29 +0800

    reverse_lined

--- linklist/reverse_linked_iteration.py
+++ linklist/reverse_linked_iteration.py
@@ -6,6 +6,8 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+
 """
 双指针迭代
 我们可以申请两个指针，第一个指针叫 pre，最初是指向 null 的。
@@ -13,6 +15,7 @@ class ListNode:
 每次迭代到 cur，都将 cur 的 next 指向 pre，然后 pre 和 cur 前进一位。
 都迭代完了(cur 变成 null 了)，pre 就是最后一个节点了。
 """
+
 
 class Solution:
     def reverseList(self, head):
@@ -30,6 +33,7 @@ class Solution:
             cur = lat
 
         return pre
+
 
 def stringToIntegerList(input):
     return json.loads(input)
