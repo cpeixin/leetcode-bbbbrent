b2590ac29958176b57de967ca37d15fcceccd871
cpeixin <421391491@qq.com>
2020-06-18 09:40:32 +0800

    add some heap

--- heap/sliding-window-maximum.py
+++ heap/sliding-window-maximum.py
@@ -1,6 +1,7 @@
 import collections
 
 
+# 双端队列
 def maxSlidingWindow(nums, k):
     """
     :type nums: List[int]
@@ -13,9 +14,9 @@ def maxSlidingWindow(nums, k):
         # deque[0]左端第一个元素， i-k ，当前窗口的左边界
         while deque and deque[0] <= i - k:
             deque.popleft()  # outdata indices 移出不属于当前窗口的元素
-        while deque and num > nums[deque[-1]]:# deque[-1]右端最后一个元素，比较当前num是否比队列头元素大。
-            deque.pop() # 队尾出一个元素
-        deque.append(i) # append 队尾添加元素
+        while deque and num > nums[deque[-1]]:  # deque[-1]右端最后一个元素，比较当前num是否比队列头元素大。
+            deque.pop()  # 队尾出一个元素
+        deque.append(i)  # append 队尾添加元素
         # 大于等于第一个窗口右边界时，则在队列头部添加最大元素
         if i >= k - 1:
             # 队列中最大的元素肯定是在头部，也就是左端。
@@ -23,24 +24,32 @@ def maxSlidingWindow(nums, k):
             res.append(nums[deque[0]])
     return res
 
-    # q, res = [], []
-    # for i in range(len(nums)):
-    #     if not q:  # 如果为空直接加入队列
-    #         q.append(i)
-    #     else:
-    #         if i == q[0] + k:  # 如果队首的索引已位于滑动窗口之外，将其出队
-    #             q.pop(0)
-    #         while q and nums[q[-1]] < nums[i]:  # 将小于当前值的队尾元素依次出队
-    #             q.pop()
-    #         q.append(i)  # 将当前值加入队列
-    #     res.append(nums[q[0]])  # 队首即最大值
-    # return res[k - 1:]  # k-1前不是有效的滑动窗口
+
+# 堆
+import heapq
+
+def maxSlidingWindow_heap(nums, k):
+    # nums = [1, 3, -1, -3, 5, 3, 6, 7]
+    # how to get max value among the window
+    # use maximum heap (-value, index)
+
+    # Time complexity : O(NlogN)
+    # Space complexity: O(N)
+
+    res, heap = [], []
+    for i in range(len(nums)):
+        heapq.heappush(heap, (-nums[i], i))
+        if i + 1 >= k:
+            while heap and heap[0][1] < i + 1 - k:
+                heapq.heappop(heap)
+            res.append(-heap[0][0])
+    return res
 
 
 if __name__ == '__main__':
-    nums = [1, 3, -1, -3, 5, 3, 6, 7]
+    nums = [1, 3, -1, -3, 5, 3, 6, 7, 1, 2, 3, 4]
     k = 3
-    result = maxSlidingWindow(nums, k)
+    result = maxSlidingWindow_heap(nums, k)
     print(result)
     # d = collections.deque()
     # d.append(1)
@@ -51,4 +60,3 @@ if __name__ == '__main__':
     # print(d[-1])
     # d.pop()
     # print(d)
-
