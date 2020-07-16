b01556edc8859c8d176045a1a54fee000de7f5b4
cpeixin <421391491@qq.com>
2020-07-17 01:26:08 +0800

    dp

--- 195/path-crossing.py
+++ 195/path-crossing.py
@@ -5,6 +5,7 @@
 # Describe ：
 import collections
 
+
 def isPathCrossing(path) -> bool:
     map_ = {'N': (0, 1), 'S': (0, -1), 'W': (-1, 0), 'E': (1, 0)}
     path_dict = collections.defaultdict(int)
@@ -21,5 +22,26 @@ def isPathCrossing(path) -> bool:
     return False
 
 
+def isPathCrossing_1(path):
+    """
+    :type path: str
+    :rtype: bool
+    : Hash map
+    """
+    if len(path) == 0: return False
+    # 定义两个hash表
+    nsew_dict = {'N': (0, 1), 'S': (0, -1), 'W': (-1, 0), 'E': (1, 0)}
+    # 原点
+    offset = (0, 0)
+    offset_array = [offset]
+    for index in path:
+        offset = (offset[0] + nsew_dict[index][0], offset[1] + nsew_dict[index][1])
+        if offset in offset_array:
+            return True
+        else:
+            offset_array.append(offset)
+    return False
+
+
 if __name__ == '__main__':
-    isPathCrossing("SS")
+    isPathCrossing_1("NES")
