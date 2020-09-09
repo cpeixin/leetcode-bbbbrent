020783796d2775248b010b0bbd8d3977b7f4bb29
cpeixin <421391491@qq.com>
2020-09-09 20:45:29 +0800

    bloom

--- data_structure/bloom_filter.py
+++ data_structure/bloom_filter.py
@@ -14,18 +14,15 @@ class BloomFilter:
         self.bit_array = bitarray(size)
         self.bit_array.serall(0)
 
-
     def add(self, data):
         for seed in range(self.hash_num):
             result = mmh3.hash(seed, data)
             self.bit_array[result] = 1
 
-
     def lookup(self, data):
         for seed in range(self.hash_num):
             result = mmh3.hash(seed, data)
             if self.bit_array[result] == 0: return 'No this data'
-
         return 'Probably'
 
 if __name__ == '__main__':
