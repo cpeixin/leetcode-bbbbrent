bce15ffb6d673e88dd89d9fb561e26128183972a
congpeixin <congpeixin@dongqiudi.com>
2021-03-18 22:06:00 +0800

    commit dongqiudi

---
+++ array/findDiagonalOrder.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/12 11:03 下午
+# @Author  : CongPeiXin
+# @Email   : congpeixin@dongqiudi.com
+# @File    : findDiagonalOrder.py
+# @Description: 
