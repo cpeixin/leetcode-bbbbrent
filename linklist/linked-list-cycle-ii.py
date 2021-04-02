2a92b4dd17a6cf0409bb6a0555001250565c2a1d
congpeixin <congpeixin@dongqiudi.com>
2021-04-02 08:27:50 +0800

    add: 环形链表

---
+++ linklist/linked-list-cycle-ii.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+"""
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-cycle
+
+"""
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def detectCycle(self, head):
+        if head is None or head.next is None: return None
+
+        slow = fast = head
+
+        while True:
+            if fast is None or fast.next is None: return None
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+
+        fast = head
+
+        while slow != fast:
+            slow, fast = slow.next, fast.next
+        return fast
