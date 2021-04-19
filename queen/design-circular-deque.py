169a1f314e602ff2711eaeb964e550cecc1bf944
congpeixin <congpeixin@dongqiudi.com>
2021-04-19 08:28:25 +0800

    队列

--- queen/design-circular-deque.py
+++ queen/design-circular-deque.py
@@ -4,36 +4,36 @@ class MyCircularQueue:
         """
         Initialize your data structure here. Set the size of the queue to be k.
         """
-        self.queue=[0]*k
-        self.headIndex=0
-        self.count=0
-        self.capacity=k
+        self.queue = [0] * k
+        self.headIndex = 0
+        self.count = 0
+        self.capacity = k
 
     def enQueue(self, value: int) -> bool:
         """
         Insert an element into the circular queue. Return true if the operation is successful.
         """
-        if self.count==self.capacity:
+        if self.count == self.capacity:
             return False
-        self.queue[(self.headIndex+self.count)%self.capacity]=value
-        self.count+=1
+        self.queue[(self.headIndex + self.count) % self.capacity] = value
+        self.count += 1
         return True
 
     def deQueue(self) -> bool:
         """
         Delete an element from the circular queue. Return true if the operation is successful.
         """
-        if self.count==0:
+        if self.count == 0:
             return False
-        self.headIndex=(self.headIndex+1)%self.capacity
-        self.count-=1
+        self.headIndex = (self.headIndex + 1) % self.capacity
+        self.count -= 1
         return True
 
     def Front(self) -> int:
         """
         Get the front item from the queue.
         """
-        if self.count==0:
+        if self.count == 0:
             return -1
         return self.queue[self.headIndex]
 
@@ -41,18 +41,18 @@ class MyCircularQueue:
         """
         Get the last item from the queue.
         """
-        if self.count==0:
+        if self.count == 0:
             return -1
-        return self.queue[(self.headIndex+self.count-1)%self.capacity]
+        return self.queue[(self.headIndex + self.count - 1) % self.capacity]
 
     def isEmpty(self) -> bool:
         """
         Checks whether the circular queue is empty or not.
         """
-        return self.count==0
+        return self.count == 0
 
     def isFull(self) -> bool:
         """
         Checks whether the circular queue is full or not.
         """
-        return self.count==self.capacity
+        return self.count == self.capacity
