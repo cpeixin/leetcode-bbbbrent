b88713613e65df22c2dde638583cf8e3a69b88e2
congpeixin <congpeixin@dongqiudi.com>
2021-04-20 08:37:51 +0800

    最小栈

--- stack/min-stack.py
+++ stack/min-stack.py
@@ -2,22 +2,31 @@
 # Author：Brent
 # Date ：2020/6/22 10:40 PM
 # Tool ：PyCharm
-# Describe ：
+# Describe ：设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+#
+# push(x) —— 将元素 x 推入栈中。
+# pop() —— 删除栈顶的元素。
+# top() —— 获取栈顶元素。
+# getMin() —— 检索栈中的最小元素。
 
 class MinStack(object):
     def __init__(self):
         self.stack = []
         self.min_stack = []
+
     def push(self, x):
         self.stack.append(x)
         # 这里注意： x <= self.min_stack[-1]    <=
         if not self.min_stack or x <= self.min_stack[-1]:
             self.min_stack.append(x)
+
     def pop(self):
         if self.stack.pop() == self.min_stack[-1]:
             self.min_stack.pop()
+
     def top(self):
         return self.stack[-1]
+
     def getMin(self):
         return self.min_stack[-1]
 
