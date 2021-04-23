cf6b2c2214b1ddebe9513bbade71ffdd47379790
congpeixin <congpeixin@dongqiudi.com>
2021-04-23 08:42:36 +0800

    单调栈

---
+++ stack/dailyTemperatures.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/23 8:14 上午
+# @Author  : CongPeiXin
+# @Email   : congpeixin@dongqiudi.com
+# @File    : dailyTemperatures.py
+# @Description:请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+#
+# 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+#
+# 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+
+# 单调栈，存储下标
+
+
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        length = len(T)
+        ans = [0] * length
+        stack = []
+        for i in range(length):
+            temperature = T[i]
+            while stack and temperature > T[stack[-1]]:
+                prev_index = stack.pop()
+                ans[prev_index] = i - prev_index
+            stack.append(i)
+        return ans
