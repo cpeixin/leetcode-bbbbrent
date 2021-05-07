d7e9a498d611aad30b77b2fe3ba4d204777c7a37
congpeixin <congpeixin@dongqiudi.com>
2021-05-07 08:54:01 +0800

    二叉树最大深度的三种做法

--- tree/max_depth.py
+++ tree/max_depth.py
@@ -5,15 +5,14 @@
 # Describe ：
 
 
-
-
 class TreeNode:
-    def __init__(self,value):
+    def __init__(self, value):
         self.val = value
         self.left = None
         self.right = None
 
 
+# 自底向上
 class Solution:
     def maxDepth(self, root):
         if not root: return 0
@@ -21,7 +20,44 @@ class Solution:
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
 
-        return max(left,right) + 1
+        return max(left, right) + 1
+
+
+# 自顶向下
+class Solution:
+    def maxDepth(self, root):
+        Solution.answer = 0
+
+        def depth(root, d):
+            if not root: return
+            if not root.left and not root.right:
+                Solution.answer = max(Solution.answer, d)
+            depth(root.left, d + 1)
+            depth(root.right, d + 1)
+
+        depth(root, 1)
+        return Solution.answer
+
+
+# 层序遍历
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        depth = 0
+        queue = [root]
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            depth += 1
+        return depth
 
 
 if __name__ == '__main__':
