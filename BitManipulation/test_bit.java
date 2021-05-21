e6fb2f3b2e309acf111cfb3c0655be4e12d380ba
congpeixin <congpeixin@dongqiudi.com>
2021-05-21 08:43:08 +0800

    找出两个数组中相同的数字

---
+++ BitManipulation/test_bit.java
@@ -0,0 +1,10 @@
+public class test_bit {
+    public static void main(String[] args) {
+        int a = 11;
+        if ((a&1)==0) {
+            System.out.println("ok");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
