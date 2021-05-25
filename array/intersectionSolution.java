ae65a110f8538884e65f440e66e0a6408387354f
congpeixin <congpeixin@dongqiudi.com>
2021-05-25 08:44:53 +0800

    hashmap

---
+++ array/intersectionSolution.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+/**
+ * 给定两个数组，编写一个函数来计算它们的交集。
+
+示例 1：
+
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2]
+
+解法： 排序 + 双指针
+ */
+
+class intersectionSolution {
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[length1 + length2];
+        int index = 0, index1 = 0, index2 = 0;
+        while (index1 < length1 && index2 < length2) {
+            int num1 = nums1[index1], num2 = nums2[index2];
+            if (num1 == num2) {
+                // 保证加入元素的唯一性
+                if (index == 0 || num1 != intersection[index - 1]) {
+                    intersection[index++] = num1;
+                }
+                index1++;
+                index2++;
+            } else if (num1 < num2) {
+                index1++;
+            } else {
+                index2++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+    public static void main(String[] args) {
+        int[] num1 = new int[]{1,1,2,2,3,4,5};
+        int[] num2 = new int[]{2,4};
+
+        int[] res = intersection(num1, num2);
+
+        System.out.println(res);
+    }
+}
---
+++ array/isHappySolution.java
@@ -0,0 +1,16 @@
+/**
+ *  Description
+    Author  
+    Date
+ */
+public class isHappy {
+
+    public static int getNext(int n){
+        int d = n / 10;
+        return d;
+    }
+    public static void main(String[] args) {
+        System.out.println(getNext(116));
+        
+    }
+}
