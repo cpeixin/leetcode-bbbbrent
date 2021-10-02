38c3b3d9dae87efedb88b1437b0fdb3261f51fa5
congpeixin <congpeixin@dongqiudi.com>
2021-10-02 10:23:42 +0800

    string

---
+++ str/reverseLeftWords.py
@@ -0,0 +1,38 @@
+# 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
+# 请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+
+#  
+
+# 示例 1：
+
+# 输入: s = "abcdefg", k = 2
+# 输出: "cdefgab"
+# 示例 2：
+
+# 输入: s = "lrloseumgh", k = 6
+# 输出: "umghlrlose"
+
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        """切片方法"""
+        return s[n:] + s[:n]
+        """列表遍历"""
+        res = []
+        for i in range(n, len(s)):
+            res.append(s[i])
+        for i in range(n):
+            res.append(s[i])
+        return ''.join(res)
+        """自己实现三段反转"""
+        def reverse_sub(lst, left, right):
+            while left < right:
+                lst[left], lst[right] = lst[right], lst[left]
+                left += 1
+                right -= 1
+        
+        res = list(s)
+        end = len(res) - 1
+        reverse_sub(res, 0, n - 1)
+        reverse_sub(res, n, end)
+        reverse_sub(res, 0, end)
+        return ''.join(res)
