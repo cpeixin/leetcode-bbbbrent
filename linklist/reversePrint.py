1437052e945ebe20e8fdfe945fd12b70b456d75a
congpeixin <congpeixin@dongqiudi.com>
2021-10-02 10:23:54 +0800

    string

--- linklist/reversePrint.py
+++ linklist/reversePrint.py
@@ -5,6 +5,9 @@ LastEditTime: 2021-10-01 08:58:15
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /leetcode-bbbbrent/linklist/reversePrint.py
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+
 '''
 
 """辅助栈"""
