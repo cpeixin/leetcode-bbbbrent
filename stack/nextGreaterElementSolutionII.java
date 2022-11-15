fcd25fb65fbd73b8c9d136ee758ff137141a2ec2
congpeixin <congpeixin@dongqiudi.com>
2022-11-15 19:28:33 +0800

    next number

---
+++ stack/nextGreaterElementSolutionII.java
@@ -0,0 +1,50 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+
+/*
+ * @Author: congpeixin congpeixin@dongqiudi.com
+ * @Date: 2022-11-14 09:17:08
+ * @LastEditors: congpeixin congpeixin@dongqiudi.com
+ * @LastEditTime: 2022-11-15 09:26:21
+ * @FilePath: /leetcode-bbbbrent/stack/nextGreaterElementSolution.java
+ * @Description: 给定一个循环数组 nums （ nums[nums.length - 1] 的下一个元素是 nums[0] ），返回 nums 中每个元素的 下一个更大元素 。
+
+数字 x 的 下一个更大的元素 是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。如果不存在，则输出 -1 。
+
+ 
+
+示例 1:
+
+输入: nums = [1,2,1]
+输出: [2,-1,2]
+解释: 第一个 1 的下一个更大的数是 2；
+     数字 2 找不到下一个更大的数； 
+     第二个 1 的下一个最大的数需要循环搜索，结果也是 2。
+示例 2:
+
+输入: nums = [1,2,3,4,3]
+输出: [2,3,4,-1,4]
+
+ */
+public class nextGreaterElementSolutionII {
+  public int[] nextGreaterElements(int[] nums) {
+    for(int i=0; i<nums.length; i++) {
+        System.out.println(nums[i]);
+        if (i==nums.length - 1) {
+            i=-1;
+        }
+    }
+    return new int[nums.length];
+  }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 4, 1, 2 };
+        int[] nums2 = { 1, 3, 4, 2 };
+        nextGreaterElementSolutionII solution = new nextGreaterElementSolutionII();
+        int[] res = solution.nextGreaterElements(nums1);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
